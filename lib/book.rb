c7cfceb9bb7e546379ff58745e192b8b14a68d50
Haleem Hamid <hhamidbiz@gmail.com>
2022-02-02 07:09:13 -0800

    commit tree a6507214f7ab8e55267530787e3db4915622653f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,33 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_reader :turn_page
 
+    def initialize(title)
+        @title= title
+    end
+    def title 
+        @title
+    end
 
+    def author
+        @author
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page 
+     puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+lost = Book.new("And Then There Were None")
